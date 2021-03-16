52e889cb1a2af98a0e24d264d3558e54efd0ad2e
Omansh5 <omanshbarnwal0360@gmail.com>
2021-03-16 11:09:27 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMergeExercise.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesign { get; set; }
+        public DateTime EDOB { get; set; }
+
+    }
+}
